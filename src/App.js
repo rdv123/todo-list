2a65d15c87c8c3cb4d447e9e90bb7dc164a6f9c5
Denis <rojkoff.d@gmail.com>
2021-09-14 23:29:09 +0300

    map list todos

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
 
   import './App.css';
 
-
   function App() {
+ const todos=
+  [{id:'1',title:'купить хеб'},
+  {id:'2',title:'выучить react'},
+  {id:'3',title:'выучить js'},
+  {id:'4',title:'выучить css'},
+  {id:'5',title:'выучить bootsrap'},
+]
+     
+
     return (
     <div className="container">
-      <button type="button" className="btn btn-primary">Кнопка1</button>
-      <form>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">Email адрес</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <div id="emailHelp" className="form-text">Введите адрес электронной почты</div>
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">Пароль</label>
-          <input type="password" className="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-          <label className="form-check-label" for="exampleCheck1">Проверить</label>
-        </div>
-        <button type="submit" className="btn btn-primary">зарегиться</button>
-      </form>
+      <div className="input-group mb-3">
+          <input type="text" 
+                   className="form-control" 
+                    placeholder="Recipient's username" 
+                aria-label="Recipient's username" 
+                  aria-describedby="button-addon2"/>
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
+      <ul className="list-group">
+      
+        {todos.map((todo)=>{
+          return  <li className="list-group-item">{todo.title}
+          <button type="button" className="btn btn-primary">Primary</button>
+          </li>
+        })}
+              
+          {/* <li className="list-group-item">A second item</li>
+          <li className="list-group-item">A third item</li>
+          <li className="list-group-item">A fourth item</li>
+          <li className="list-group-item">And a fifth one</li> */}
+        </ul>
     </div>
-    
     
   
     );
