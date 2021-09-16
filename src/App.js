e1aaab4907f02cea8db35157a65a656192a67f15
Denis <rojkoff.d@gmail.com>
2021-09-16 22:17:39 +0300

    add-delete

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@
 import './App.css';
 
   function App() {
+    const [text,setText] = useState('')
+    
    const [todos,setTodos] = useState( 
    [{id:'1',title:'купить хеб'},
    {id:'2',title:'выучить react'},
@@ -10,36 +12,69 @@ import './App.css';
    {id:'4',title:'выучить css'},
    {id:'5',title:'выучить bootsrap'},
  ])
-
-  const [color,setColor] = useState('black')
-   let colorTest ='blackTest'
+//  const [count, setCount] = useState(0)
+//  (count) => {
+//   setCount(count+1)
+//  }
+  let count = 0
+ count++
+ let pushTask = (title)=>{
+   
+  //  const copyArray = [...todos]
+  //  console.log('copyArray',copyArray)
+  //  copyArray.push({id:'6',title})
+   setTodos([{id:count,title},...todos])
+   console.log(...todos)
+  setText("")
+  //  setTodos()
+  //  console.log(todos)
+  //  todos.unshift({id:'6',title})
+  //  console.log(todos)
+ }
+  // const [color,setColor] = useState('black')
+  //  let colorTest ='blackTest'
  
     let  deleteTask = (id) => {
-        colorTest ='green'
-        console.log(colorTest)
-        setColor('blue')
+      setTodos([...todos].filter((todo)=>todo.id !== id))
+  //     const copyArray = [...todos]
+  //  console.log('copyArray',copyArray)
+  //  copyArray.filter((todo) => todo.id !== id)
+  //  setTodos(copyArray)
+  //  console.log(copyArray)
+        // colorTest ='green'
+        // console.log(colorTest)
+        // setColor('blue')
+      // let newTodos = todos.filter((todo) => todo.id !== id)
+      // console.log(newTodos)
+      
     }
 
     return (
     <div className="container">
       <div className="input-group mb-3">
           <input type="text" 
+               value={text}
                    className="form-control" 
-                    placeholder="Recipient's username" 
-                aria-label="Recipient's username" 
-                  aria-describedby="button-addon2"/>
-        <button type="button" className="btn btn-primary">Primary</button>
+                    placeholder="Recipi" 
+                aria-label="Recipient's us" 
+                  aria-describedby="button-addon2"
+                   onChange={
+                     (e)=>setText(e.target.value)
+                    }
+                    />
+        <button type="button" 
+                className="btn btn-primary"
+                onClick = {()=> pushTask (text)}>Добавить</button>
       </div>
       <ul className="list-group">
       
         {todos.map((todo)=>{
-          return  <div><li className="list-group-item">{todo.title}
-          
-          </li>
-          <button type="button" 
-                  className="btn btn-primary"
-                  onClick={()=>deleteTask(todo.id)}>{color} -------{colorTest}</button>
-          </div>
+          return  <div>
+                      <li className="list-group-item">{todo.title}</li>
+                      <button type="button" 
+                            className="btn btn-primary"
+                            onClick={()=>deleteTask(todo.id)}>Удалить</button>
+                      </div>
         })}
               
           {/* <li className="list-group-item">A second item</li>
