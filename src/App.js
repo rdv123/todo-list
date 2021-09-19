310d5796a9838c40e0b4c44a9fbb2fde9cb3b294
Denis <rojkoff.d@gmail.com>
2021-09-19 11:01:59 +0300

    strikeOutText

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 
   import { useState } from 'react';
 import './App.css';
-
+import Todo from './component/Todo';
+let count = 5
   function App() {
     const [text,setText] = useState('')
     
    const [todos,setTodos] = useState( 
-   [{id:'1',title:'купить хеб'},
-   {id:'2',title:'выучить react'},
-   {id:'3',title:'выучить js'},
-   {id:'4',title:'выучить css'},
-   {id:'5',title:'выучить bootsrap'},
+   [{id:'1',title:'купить хеб',date:'13.09.21', marked: false},
+   {id:'2',title:'выучить react', date:'13.09.21', marked: false},
+   {id:'3',title:'выучить js', date:'13.09.21', marked: true},
+   {id:'4',title:'выучить css', date:'13.09.21', marked: false},
+   {id:'5',title:'выучить bootsrap',date:'13.09.21', marked: true},
  ])
 //  const [count, setCount] = useState(0)
 //  (count) => {
 //   setCount(count+1)
 //  }
-  let count = 0
- count++
+
+ 
  let pushTask = (title)=>{
-   
+  count++
   //  const copyArray = [...todos]
   //  console.log('copyArray',copyArray)
   //  copyArray.push({id:'6',title})
-   setTodos([{id:count,title},...todos])
-   console.log(...todos)
+   setTodos([{id:count.toString(),title},...todos])
+  
   setText("")
+  console.log(todos)
+
   //  setTodos()
   //  console.log(todos)
   //  todos.unshift({id:'6',title})
@@ -48,33 +51,57 @@ import './App.css';
       // console.log(newTodos)
       
     }
-
+    let strikOutText = (id) => {
+   console.log(id)
+    //  const todo = todos.find((todo)=>todo.id === id)
+    // //  console.log('todo',todo)
+    // todo.marked = !todo.marked
+    const newTodos= todos.map((todo)=>{
+      if(todo.id ===id){
+        todo.marked = !todo.marked
+      }
+      return todo
+    })
+     setTodos(newTodos)
+    }
     return (
-    <div className="container">
+    <div className="container mt-3">
+ 
+ 
+   
+
       <div className="input-group mb-3">
           <input type="text" 
                value={text}
                    className="form-control" 
-                    placeholder="Recipi" 
+                    placeholder="Внесите задачу" 
                 aria-label="Recipient's us" 
                   aria-describedby="button-addon2"
                    onChange={
                      (e)=>setText(e.target.value)
                     }
                     />
-        <button type="button" 
+                     <button type="button" 
                 className="btn btn-primary"
                 onClick = {()=> pushTask (text)}>Добавить</button>
-      </div>
-      <ul className="list-group">
       
+      </div>
+     
+      {/* <div class="hstack gap-3">
+  <input class="form-control me-auto" type="text" placeholder="Добавьте сюда свой товар...">
+  <button type="button" class="btn btn-secondary">Отправить</button>
+  <div class="vr"></div>
+  <button type="button" class="btn btn-outline-danger">Сбросить</button>
+</div> */}
+     <div> 
+      <ul className="list-group">
         {todos.map((todo)=>{
-          return  <div>
-                      <li className="list-group-item">{todo.title}</li>
-                      <button type="button" 
-                            className="btn btn-primary"
-                            onClick={()=>deleteTask(todo.id)}>Удалить</button>
+        return  (
+        <div className='d-flex mb-3 shadow' key={todo.id}>
+                    <Todo deleteTaskProps={deleteTask} strikOutText={strikOutText}  todoProps={todo}  />
                       </div>
+                      
+                      )
         })}
               
           {/* <li className="list-group-item">A second item</li>
@@ -82,6 +109,7 @@ import './App.css';
           <li className="list-group-item">A fourth item</li>
           <li className="list-group-item">And a fifth one</li> */}
         </ul>
+        </div>
     </div>
     
   
---
+++ src/component/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Todo = ({todoProps,deleteTaskProps, strikOutText}) => {
+   
+    return (
+        <>
+            <li className="list-group-item w-100 d-flex" 
+                onClick = {()=>strikOutText(todoProps.id)}
+                style={{ textDecorationLine: todoProps.marked ? 'line-through' : ''}}>{todoProps.title} 
+                <small className="ms-auto">{todoProps.date}</small> 
+            </li>
+                <button type="button" 
+                        className="btn btn-danger"                       
+             onClick={()=>deleteTaskProps(todoProps.id)}>Удалить</button>
+        </>
+    )
+}
+
+export default Todo
